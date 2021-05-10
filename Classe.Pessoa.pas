9b07602370720dbcd9aedbee42a54ddded4accf5
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2021-05-10 20:30:02 -0300

    Adicionado Form principal e classe pessoa

---
+++ Classe.Pessoa.pas
@@ -0,0 +1,15 @@
+unit Classe.Pessoa;
+
+interface
+
+type
+  TPessoa = class
+  private
+
+  public
+    property Nome : String;
+  end;
+
+implementation
+
+end.
---
+++ OOP_Projeto.dpr
@@ -0,0 +1,15 @@
+program OOP_Projeto;
+
+uses
+  Vcl.Forms,
+  View.Principal in 'View.Principal.pas' {frmPrincipal},
+  Classe.Pessoa in 'Classe.Pessoa.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
